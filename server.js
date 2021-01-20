a2f13b81c90255b2eb155ec14113f2e80d2326a6
mbool <mstybowski@gmail.com>
2021-01-20 17:28:32 +0100

    added csv saving feature

--- server.js
+++ server.js
@@ -2,14 +2,48 @@ const express = require("express")
 const app = express();
 const cors = require("cors")
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
 let port = 8888;
 
 app.use(cors())
+
+function saveToCSV(array){
+
+
+
+    let myArrayOfStrings = array.split(',');
+    console.log(myArrayOfStrings);
+
+    let arrayOfValues = myArrayOfStrings.map(function(x) {
+        return {value:parseInt(x)}
+    });
+
+    console.log(arrayOfValues);
+
+    const csvWriter = createCsvWriter({
+        path: 'out.csv',
+        header: [
+            {id: 'value', title: 'Value'}
+        ]
+    });
+
+
+    csvWriter
+        .writeRecords(arrayOfValues)
+        .then(()=> console.log('The CSV file was written successfully'));
+
+}
 
 app.get("/test", (req, res) =>{
 
     res.send(req.header("myHeader"))
     console.log("HEADER: " + req.header("myHeader"));
+
+    if(req.header("delays")){
+        console.log(req.header("delays"))
+        saveToCSV(req.header("delays"))
+    }
 })
 
 app.listen(port, (err) => {
